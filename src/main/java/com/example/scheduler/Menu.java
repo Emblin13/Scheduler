6ed452f4631993dff73cf72dfbfff5a77b87ebf9
Hunter Allen <hallen10@ewu.edu>
2023-05-01 10:28:42 -0700

    Added manager and employee button to the main menu.

--- src/main/java/com/example/scheduler/Menu.java
+++ src/main/java/com/example/scheduler/Menu.java
@@ -1,10 +1,17 @@
 package com.example.scheduler;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Menu {
 
     Stage stage;
+    Button managerButton;
+    Button employeeButton;
+    VBox layout;
+    Scene scene;
 
     public Menu(){
         System.out.println("Menu created");
@@ -13,8 +20,24 @@ public class Menu {
     public void start(Stage stage){
         System.out.println("Menu started");
         this.stage = stage;
+
+        managerButton = new Button("Manager");
+        employeeButton = new Button("Employee");
+        managerButton.setOnAction(e -> managerButtonClicked());
+
+        layout = new VBox(20);
+        layout.getChildren().addAll(managerButton, employeeButton);
+        layout.setAlignment(javafx.geometry.Pos.CENTER);
+        scene = new Scene(layout, 300, 250);
+        stage.setScene(scene);
+
         stage.show();
     }
 
+    public void managerButtonClicked(){
+        System.out.println("Manager button clicked");
+        Scheduler scheduler = new Scheduler();
+        scheduler.start(stage);
+    }
 
 }
--- src/main/java/com/example/scheduler/Scheduler.java
+++ src/main/java/com/example/scheduler/Scheduler.java
@@ -1,7 +1,17 @@
 package com.example.scheduler;
 
+import javafx.stage.Stage;
+
 public class Scheduler {
+    Stage stage;
+
     public Scheduler(){
         System.out.println("Scheduler created");
     }
+
+    public void start(Stage stage){
+        System.out.println("Scheduler started");
+        this.stage = stage;
+    }
+
 }
