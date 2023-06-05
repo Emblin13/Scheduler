49271d47f5a9981ab5908430b57ce3e1b59c7199
BrendanK <brendan_emblin@hotmail.com>
2023-06-05 16:39:34 -0700

    Possible fix for compiler issues

--- src/main/java/com/example/scheduler/ScreenShotter.java
+++ src/main/java/com/example/scheduler/ScreenShotter.java
@@ -1,4 +1,4 @@
-package ScreenShot;
+package com.example.scheduler;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.scheduler {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.desktop;
 
 
     opens com.example.scheduler to javafx.fxml;
